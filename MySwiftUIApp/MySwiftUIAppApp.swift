4aae1bb8db976e2779bd28fb6738be5bb48a9b8c
Durga077 <peddi.durgaa@gmail.com>
2024-02-22 15:22:33 +0530

    Initial Commit

---
+++ MySwiftUIApp/MySwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUIAppApp.swift
+//  MySwiftUIApp
+//
+//  Created by User on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
